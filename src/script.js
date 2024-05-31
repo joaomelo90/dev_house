7f3c09efba03b725f17a879713dfdb0ccf88e3c0
joaomelo90 <joaopaulomoreiramelo@gmail.com>
2024-05-31 20:20:44 -0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,13 @@
+const menuMobile = document.getElementById("menu")
+
+function openMenu(){
+    menuMobile.classList.remove("hidden")
+    menuMobile.classList.add("flex")
+}
+
+function closeMenu(){
+    menuMobile.classList.remove("flex")
+    menuMobile.classList.add("hidden")
+}
+
+
